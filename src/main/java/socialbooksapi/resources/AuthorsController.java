e151e72d6f6f9d015f0e8b94d6cad19064496cd1
Thigo Medeiros <si-thiago@hotmail.com>
2018-12-05 23:44:14 -0300

    Changing the representation type (format of response, XML or JSON)

--- src/main/java/socialbooksapi/resources/AuthorsController.java
+++ src/main/java/socialbooksapi/resources/AuthorsController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +25,9 @@ public class AuthorsController {
 	@Autowired
 	private AuthorsService authorsService;
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET, produces={
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE
+	})
 	public ResponseEntity<List<Author>> findAll() {
 		List<Author> authors = authorsService.findAll();
 		return ResponseEntity.status(HttpStatus.OK).body(authors);
