c0f28dd3e522dc56155fedb0e87dfd205058e91f
Thigo Medeiros <si-thiago@hotmail.com>
2018-12-06 00:28:44 -0300

    Protecting our routes by authentication

---
+++ src/main/java/socialbooksapi/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package socialbooksapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("tomurlh").password("tomurlh").roles("USER");
+	}
+	
+	
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/h2-console/**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+			.httpBasic()
+		.and()
+			.csrf().disable();
+		
+	}
+}
--- src/main/java/socialbooksapi/resources/BooksController.java
+++ src/main/java/socialbooksapi/resources/BooksController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -87,13 +89,18 @@ public class BooksController {
 		
 		return ResponseEntity.status(HttpStatus.OK).body(comments);
 	}
-	
 
-	
-		
+
+
+
 	@RequestMapping(value="/{id}/comments", method=RequestMethod.POST)
 	public ResponseEntity<Void> addComment(@PathVariable("id") Long bookId,
 			@RequestBody Comment comment) {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		comment.setUser(auth.getName());
+		
 		booksService.saveComment(bookId, comment);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
