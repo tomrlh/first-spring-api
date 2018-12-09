436bebca8abe6295c77354bf630e12b2c8823e8a
Thigo Medeiros <si-thiago@hotmail.com>
2018-12-09 11:59:51 -0300

    Adding CrossOrigin + refactoring BooksController

--- src/main/java/socialbooksapi/resources/BooksController.java
+++ src/main/java/socialbooksapi/resources/BooksController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,7 @@ public class BooksController {
 	@Autowired
 	private BooksService booksService;
 
+	@CrossOrigin
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<Book>> findAll() {
 		return ResponseEntity.status(HttpStatus.OK).body(booksService.findAll());
